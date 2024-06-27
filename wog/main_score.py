588645ea08544c50d674c4b8d9c5d2dbab719e5a
dorsh97 <dor223@gmail.com>
2024-06-27 19:25:53 +0300

    fixed indent on main_score.py

--- wog/main_score.py
+++ wog/main_score.py
@@ -13,30 +13,30 @@ def score_server():
             @app.route("/")
             def flask():
                 return f"""
-                <html>
-                    <head>
-                        <title>Scores Game</title>
-                    </head>
-                    <body>
-                        <h1>The Score is:</h1>             
-                        <div id="score">{score}</div>
-                    </body>
-                </html>
-        """
+                    <html>
+                        <head>
+                            <title>Scores Game</title>
+                        </head>
+                        <body>
+                            <h1>The Score is:</h1>             
+                            <div id="score">{score}</div>
+                        </body>
+                    </html>
+                    """
         else:
             score = BAD_RETURN_CODE
     if not os.path.exists(SCORES_FILE_NAME) or score == BAD_RETURN_CODE:
         @app.route("/")
         def flask():
             return f"""
-            <html>
-                <head>
-                    <title>Scores Game</title>
-                </head>
-                <body>
-                    <h1>ERROR:</h1>             
-                    <div id="score" style="color:red">{BAD_RETURN_CODE}</div>
-                </body>
-            </html>
-    """
+                <html>
+                    <head>
+                        <title>Scores Game</title>
+                    </head>
+                    <body>
+                        <h1>ERROR:</h1>             
+                        <div id="score" style="color:red">{BAD_RETURN_CODE}</div>
+                    </body>
+                </html>
+                """
     return app.run("0.0.0.0")
