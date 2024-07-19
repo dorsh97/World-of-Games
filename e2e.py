c596cfef75a64555f9859f3c96dd2be8164fc5f1
dorsh97 <dor223@gmail.com>
2024-07-19 14:12:46 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager  # pip install webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import ChromiumOptions
 from selenium.webdriver.common.by import By
 
