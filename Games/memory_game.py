f5d86c4e9ce4d93be56aeccbad69510a6569f23c
dorsh97 <dor223@gmail.com>
2024-07-21 10:34:07 +0300

    Update memory_game.py

--- Games/memory_game.py
+++ Games/memory_game.py
@@ -1,6 +1,6 @@
 import random
 import time
-
+from Utilities.utils import screen_cleaner
 
 def generate_sequence(game_diff):
     return [random.randint(1, 100) for i in range(game_diff)]
@@ -20,10 +20,9 @@ def is_list_equal(rand_list, user_list):
 def play(game_diff):
     rand_list = generate_sequence(game_diff)
     time.sleep(0.7)
-    print(rand_list, end="")
+    print(rand_list, end="", flush=True)
     time.sleep(0.7)
-    for i in range(len(str(rand_list))):
-        print("\b", end="")
+    screen_cleaner(rand_list)
     time.sleep(0.7)
     user_list = get_list_from_user(game_diff, rand_list)
     return is_list_equal(rand_list, user_list)
